351e701f45b11eeacc0f1df2651ecf695ac01e1f
CarlosJX7 <carlos.jx7@gmail.com>
2026-06-01 02:00:49 +0200

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -64,32 +64,14 @@ int	print_output(char *str, va_list list)
 	return (printed);
 }
 
-int	count_format(char const *str)
-{
-	size_t	i;
-	size_t	n;
-
-	i = 0;
-	n = 0;
-	while (str[i])
-	{
-		if (str[i] == '%')
-			n++;
-		i++;
-	}
-	return (n);
-}
-
 int	ft_printf(char const *format, ...)
 {
 	va_list	list;
-	size_t	n;
 	char	*str;
 	int		printed;
 
 	printed = 0;
 	str = (char *)format;
-	n = count_format(format);
 	va_start(list, format);
 	printed = print_output(str, list);
 	va_end(list);
